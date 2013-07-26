85f74f79e63e08578121dd503372de4f02292021
Simon Thum <simon.thum@gmx.de>
2013-07-26 19:50:19 +0200

    test transpose

--- de.fhg.igd.pcolor.test/src/de/fhg/igd/pcolor/test/TranspositionTest.java
+++ de.fhg.igd.pcolor.test/src/de/fhg/igd/pcolor/test/TranspositionTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import de.fhg.igd.pcolor.CIEXYZ;
+import de.fhg.igd.pcolor.Illuminant;
 import de.fhg.igd.pcolor.JCh;
 import de.fhg.igd.pcolor.Jab;
 import de.fhg.igd.pcolor.PColor;
@@ -11,6 +13,10 @@ import de.fhg.igd.pcolor.sRGB;
 import de.fhg.igd.pcolor.colorspace.CS_JCh;
 import de.fhg.igd.pcolor.colorspace.CS_Jab;
 import de.fhg.igd.pcolor.colorspace.CS_sRGB;
+import de.fhg.igd.pcolor.colorspace.Surrounding;
+import de.fhg.igd.pcolor.colorspace.ViewingConditions;
+import de.fhg.igd.pcolor.util.ColorTools;
+import de.fhg.igd.pcolor.util.MathTools;
 
 // these could probably become more precise, but seemingly CIECAM02
 // itself has some open issues with numerical stability. Until these
@@ -39,6 +45,52 @@ public class TranspositionTest {
 		assertEquals(rgb.get(sRGB.R), 1.0, 0.01);
 		assertEquals(rgb.get(sRGB.G), 1.0, 0.01);
 		assertEquals(rgb.get(sRGB.B), 1.0, 0.01);
+	}
+	
+	/***********************************************
+	 * Transposition tests
+	 * 
+	 * These try to reproduce the results shown on
+	 * http://scanline.ca/ciecam02/
+	 * related to transposing colors.
+	 **********************************************/
+	@Test
+	public void matchResultsGreyscale() {
+		sRGB[] p1 = new sRGB[] {ColorTools.parseColor("#444"), ColorTools.parseColor("#5d5f5f")};
+		sRGB[] p2 = new sRGB[] {ColorTools.parseColor("#888"), ColorTools.parseColor("#9c9fa0")};
+		sRGB[] p3 = new sRGB[] {ColorTools.parseColor("#ccc"), ColorTools.parseColor("#d4d8d9")};
+		
+		// create office roughly VC following CIE 159:2004 sec. 5
+		float[] mixedWhitepoint = ViewingConditions.mixedWhitepoint(Illuminant.D65, Illuminant.F2, 1f).toxyY();
+		// scene white luminance
+		ViewingConditions vcWhite = ViewingConditions.createAdapted(
+				CIEXYZ.fromxyY(mixedWhitepoint[0], mixedWhitepoint[1], 80f), 16, 159, Surrounding.averageSurrounding);
+		CS_JCh cs_white = new CS_JCh(vcWhite);
+		
+		ViewingConditions vcBlack = ViewingConditions.createAdapted(
+				CIEXYZ.fromxyY(mixedWhitepoint[0], mixedWhitepoint[1], 40f), 20/*very invariant*/, 159, Surrounding.averageSurrounding);
+		CS_JCh cs_black = new CS_JCh(vcBlack);
+		
+		// assertArrayEquals(p1[0].getComponents(), ColorTools.parseColor("#444").getComponents(), 0.00001f);
+		
+		testTransposition(p1[0], p1[1], cs_white, cs_black, 0.015f);
+		testTransposition(p2[0], p2[1], cs_white, cs_black, 0.015f);
+		testTransposition(p3[0], p3[1], cs_white, cs_black, 0.015f);
+	}
+
+	// test that g1 becomes g2 when transposed from cs1 to cs2 
+	private void testTransposition(sRGB g1, sRGB g2, CS_JCh cs1, CS_JCh cs2, float d) {
+		// to JCh w/ black BG
+		JCh jch1 = new JCh(g1, cs2);
+		
+		// transpose
+		jch1 = (JCh) jch1.transpose(cs1);
+		
+		// to RGB
+		sRGB c2 = (sRGB) PColor.convert(jch1, CS_sRGB.instance);
+		
+		// compare
+		assertArrayEquals(g2.getComponents(), c2.getComponents(), d);
 	}
 	
 	
